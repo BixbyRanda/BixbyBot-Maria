1c1d5242658c0f6ee8e001df9659f28698579d2d
BixbyRanda <randazzod927@gmail.com>
2024-08-23 19:36:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],	
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],
-  ['xxxxxxxxxx'],
+  ['393201391411', 'Spidy🕷🕸💋', true],
 ] //Numero degli owner
 
 //────────────────────────────
